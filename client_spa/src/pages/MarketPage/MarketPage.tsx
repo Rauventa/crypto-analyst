76b30707d9fc4eb6b566b952ef1a7ebff6c95f67
Илья Моисеенко <Ilya>
2021-12-31 16:32:33 +0300

    coins get

--- client_spa/src/pages/MarketPage/MarketPage.tsx
+++ client_spa/src/pages/MarketPage/MarketPage.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from 'axios'
 
 export const MarketPage = () => {
+
+  const fetchData = async () => {
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
       MarketPage
